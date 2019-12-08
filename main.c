9a6736dcfe8d902d66ef4d7bdd8b58e549b5f513
Yuri Rider Sarah <yuri-rider-sarah@protonmail.com>
2019-12-08 17:18:05 +0100

    Added PCI device detection

--- main.c
+++ main.c
@@ -27,6 +27,25 @@ void kernel_main(void) {
     page_alloc_init();
     ps2_init();
 
+    for (u32 addr = 0x80000000; addr < 0x80FFF800; addr += 0x800) {
+        u32 data;
+        u32 class;
+        asm volatile ("out %1, %0" : : "a"(addr), "Nd"((u16)0xCF8));
+        asm volatile ("ind %0, %1" : "=a"(data) : "Nd"((u16)0xCFC));
+        asm volatile ("out %1, %0" : : "a"(addr + 0x08), "Nd"((u16)0xCF8));
+        asm volatile ("ind %0, %1" : "=a"(class) : "Nd"((u16)0xCFC));
+        u16 vendor_id = data >> 16;
+        if (vendor_id != 0xFFFF) {
+            print_string(STR("Valid PCI device at bus "));
+            print_hex((addr >> 16) & 0xFF, 2);
+            print_string(STR(", number "));
+            print_hex((addr >> 11) & 0x1F, 2);
+            print_string(STR(", class etc. "));
+            print_hex(class, 8);
+            print_char('\n');
+        }
+    }
+
     print_string(STR("HomuOS\n\nKeyboard test:\n"));
     u8 key;
     while (true) {
